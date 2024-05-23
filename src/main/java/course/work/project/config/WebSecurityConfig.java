c12beb77302cfec9e61dbbd90d5f4d7c26d817f7
RubezhkinLabs <pavel-ned-03@mail.ru>
2024-05-23 18:39:43 +0300

    modified:   src/main/java/course/work/project/config/WebSecurityConfig.java

--- src/main/java/course/work/project/config/WebSecurityConfig.java
+++ src/main/java/course/work/project/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
 			.dataSource(dataSource)
-			.passwordEncoder([NoOpPasswordEncoder.getInstance()])
+			.passwordEncoder(NoOpPasswordEncoder.getInstance())
 			.usersByUsernameQuery("select username, password, active from usr where username=?")
 			.authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username = ?");
 	}
