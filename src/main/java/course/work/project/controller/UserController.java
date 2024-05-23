b3643e41bfed16f6671de5ec94cf87d45a4ab6b6
RubezhkinLabs <pavel-ned-03@mail.ru>
2024-05-23 18:38:54 +0300

    modified:   src/main/java/course/work/project/config/WebSecurityConfig.java 	new file:   src/main/java/course/work/project/controller/UserController.java 	modified:   src/main/resources/templates/developers.html 	modified:   src/main/resources/templates/edit_developer.html 	modified:   src/main/resources/templates/edit_game.html 	modified:   src/main/resources/templates/edit_genre.html 	new file:   src/main/resources/templates/edit_user.html 	modified:   src/main/resources/templates/game.html 	modified:   src/main/resources/templates/games.html 	modified:   src/main/resources/templates/games_admin.html 	modified:   src/main/resources/templates/genres.html 	modified:   src/main/resources/templates/greeting.html 	modified:   src/main/resources/templates/new_developer.html 	modified:   src/main/resources/templates/new_game.html 	modified:   src/main/resources/templates/new_genre.html 	new file:   src/main/resources/templates/users.html

---
+++ src/main/java/course/work/project/controller/UserController.java
@@ -0,0 +1,63 @@
+package course.work.project.controller;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import course.work.project.entity.Genre;
+import course.work.project.entity.Role;
+import course.work.project.entity.User;
+import course.work.project.repository.GenreRepository;
+import course.work.project.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@AllArgsConstructor
+@Controller
+public class UserController {
+	@Autowired
+	private UserRepository repository;
+
+	@GetMapping("/admin/users")
+	public String getUsers(Model model) {
+		model.addAttribute("users", repository.findAll());
+		return "users";
+	}
+
+	@GetMapping("/admin/users/{id}/edit")
+	public String editUser(@PathVariable Long id, Model model) {
+		User user = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid User id: " + id));
+		model.addAttribute("user", user);
+		return "edit_user";
+	}
+	
+	@PostMapping("/admin/users/{id}")
+public String updateUser(@PathVariable Long id, @RequestParam("username") String username, @RequestParam(value = "isAdmin", required = false) boolean isAdmin) {
+    User user = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid User id: " + id));
+    user.setUsername(username);
+	Set<Role> roles = new LinkedHashSet<>();
+	roles.add(Role.USER);
+    if(isAdmin)
+		roles.add(Role.ADMIN);
+	
+	user.setRoles(roles);
+    repository.save(user); // Сохраняем обновленного пользователя
+    return "redirect:/admin/users";
+}
+
+	@GetMapping("/admin/users/{id}/delete")
+    public String deleteUser(@PathVariable Long id) {
+        repository.deleteById(id);
+        return "redirect:/admin/users";
+    }
+}
